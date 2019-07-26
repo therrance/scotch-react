1b84c37e4f97d2ad836872eb1aef27e2ab961ad8
tgordienko <tgordienko@larch-networks.com>
2019-07-26 18:39:57 +0300

    Product list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 // import logo from "./logo.svg";
 import "./App.scss";
 
-function App() {
-  const isActive = true;
-  const headerColor = isActive ? "green" : "red";
-
-  return React.createElement(
-    "h1",
-    { className: "whoa" },
-    "WHOA",
-    React.createElement("span", null, headerColor)
+const ProcuctCategoryRow = props => {
+  return (
+    <tr>
+      <th colSpan="2">{props.category}</th>
+    </tr>
   );
-}
+};
 
-export default App;
+const ProductRow = props => {
+  let name = props.product.stocked ? (
+    props.product.name
+  ) : (
+    <span style={{ color: "red" }}>{props.product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{props.product.price}</td>
+    </tr>
+  );
+};
+
+const ProductTable = props => {
+  let rows = [];
+  let lastCategory = null;
+
+  props.products.forEach(product => {
+    if (
+      product.name.indexOf(props.filterText) === -1 ||
+      (!product.ctocked && props.inStockOnly)
+    ) {
+      return null;
+    }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProcuctCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
+
+const SearchBar = props => {
+  let filterTextInput, inStockOnlyInput;
+  const handleChange = () => {
+    props.onUserInput(filterTextInput.value, inStockOnlyInput.checked);
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search.."
+        value={props.filterText}
+        ref={input => (filterTextInput = input)}
+        onChange={handleChange}
+      />
+      <p>
+        <input
+          type="checkbox"
+          checked={props.inStockOnly}
+          ref={input => (inStockOnlyInput = input)}
+          onChange={handleChange}
+        />{" "}
+        Only show products in stock
+      </p>
+    </form>
+  );
+};
+
+const FilterableProductTable = props => {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  const handleUserInput = (filterText, inStockOnly) => {
+    setFilterText(filterText);
+    setInStockOnly(inStockOnly);
+  };
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onUserInput={handleUserInput}
+      />
+      <ProductTable
+        products={props.products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+};
+
+export default FilterableProductTable;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import FilterableProductTable from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-// const message = "I'm here to save the day";
-// ReactDOM.render(<h1>Message: {message}</h1>, document.getElementById("root"));
+const PRODUCTS = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball"
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch"
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5"
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
+];
+
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
